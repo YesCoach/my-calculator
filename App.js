bb70005ca6be48c8374d6081503daadfd185e5d9
yescoach <sbsii1020@gmail.com>
2024-06-24 17:51:18 +0900

    feat: 계산기 기본 로직 구현

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
 import Calculator from './src/Calculator';
 
 export default function App() {
-  const [input, setInput] = useState(0); // 입력창
-  const [currentOperator, setCurrentOperator] = useState(null); // 연산자 볼드처리
-  const [result, setResult] = useState(null); // 결과창
-  const [tempInput, setTempInput] = useState(null); // 연산에 사용될 값
-  const [tempOperator, setTempOperator] = useState(null); // 연산에 사용될 연산자
-
   return (
     // SafeArea
     <SafeAreaView style={styles.container}>
